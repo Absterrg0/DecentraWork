98ae8185ae1a5e314d434091d59043e7e2473553
Absterrg0 <Parvj5212@gmail.com>
2024-11-04 14:15:35 +0530

    Cant_get_it_to_work_on_mainnet

--- app/projects/[id]/feature/page.tsx
+++ app/projects/[id]/feature/page.tsx
@@ -4,7 +4,7 @@ import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import FeaturePaymentComponent from '@/components/FeaturePayment';
 export default function SolanaPayment() {
   return (
-    <ConnectionProvider endpoint='https://thrumming-little-card.solana-mainnet.quiknode.pro/43538a133d29fd9eecf9318a37ee158e10d9bb34'>
+    <ConnectionProvider endpoint='https://api.devnet.solana.com/'>
       <WalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>
         <FeaturePaymentComponent ></FeaturePaymentComponent>
